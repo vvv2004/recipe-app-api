3aef263aec1751b04f7e51a2d3159bb6154235a3
vvv2004 <viktorvalentinov14@abv.bg>
2023-09-07 13:26:39 +0300

    linting fixed

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,10 @@
 Calculation functions
 """
 
-def add(x, y):
+def add(x, y): 
     """Add two numbers together"""
     return x + y
 
-def subtract(x, y):
+def subtract(x, y): 
     """Subtract x from y and return value"""
     return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -5,16 +5,14 @@ Sample tests
 from django.test import SimpleTestCase
 from . import calc
 
-class CalcTests(SimpleTestCase):
+class CalcTests(SimpleTestCase): 
     """Test the calc module"""
     def test_add_numbers(self):
         """Test that two numbers are added together"""
         res = calc.add(3, 8)
-        
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
         """Test that values are subtracted and returned"""
         res = calc.subtract(5, 11)
-        
         self.assertEqual(res, -6)
