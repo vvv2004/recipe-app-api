fa5fd32181691fee4ae8ca444c37a60a0df93d4a
vvv2004 <viktorvalentinov14@abv.bg>
2023-09-11 17:56:14 +0300

    ingredient features and tests implemented

--- app/core/models.py
+++ app/core/models.py
@@ -62,6 +62,7 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField("Tag")
+    ingredients = models.ManyToManyField("Ingredient")
 
     def __str__(self):
         return self.title
@@ -69,6 +70,19 @@ class Recipe(models.Model):
 
 class Tag(models.Model):
     """Tag for filtering recipes"""
+
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredient for recipes"""
 
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
---
+++ app/recipe/tests/test_ingredients.py
@@ -0,0 +1,99 @@
+"""
+Test for the Ingredient API
+"""
+
+from decimal import Decimal
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Recipe, Ingredient
+
+from recipe.serializers import IngredientSerializer
+
+
+INGREDIENTS_URL = reverse("recipe:ingredient-list")
+
+
+def detail_url(ingredient_id):
+    """Create and return an ingredient detail URL"""
+    return reverse("recipe:ingredient-detail", args=[ingredient_id])
+
+
+def create_user(email="user@example.com", password="testpass123"):
+    return get_user_model().objects.create_user(email=email, password=password)
+
+
+class PublicIngredientsApiTests(TestCase):
+    """Test unauthenticated API requests"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        """Test auth is required for retrieving ingredients"""
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateIngredientsApiTest(TestCase):
+    """Test unauthenticated API requests"""
+
+    def setUp(self):
+        self.user = create_user()
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_ingredients(self):
+        """Test retrieving ingredients"""
+        Ingredient.objects.create(user=self.user, name="Kale")
+        Ingredient.objects.create(user=self.user, name="Vanilla")
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        ingredients = Ingredient.objects.all().order_by("-name")
+        serializer = IngredientSerializer(ingredients, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_ingredients_limited_to_user(self):
+        """List of ingredients is limited to authenticated user"""
+        user2 = create_user(email="example@user.com")
+        Ingredient.objects.create(user=user2, name="Salt")
+        ingredient = Ingredient.objects.create(user=self.user, name="Pepper")
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]["name"], ingredient.name)
+        self.assertEqual(res.data[0]["id"], ingredient.id)
+
+    def test_update_ingredient(self):
+        """Test ingredient update"""
+        ingredient = Ingredient.objects.create(user=self.user, name="Cilantro")
+
+        payload = {"name": "Coriander"}
+        url = detail_url(ingredient.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        ingredient.refresh_from_db()
+        self.assertEqual(ingredient.name, payload["name"])
+
+    def test_delete_ingredient(self):
+        """Test deleting an ingredient"""
+        ingredient = Ingredient.objects.create(user=self.user, name="Sugar")
+
+        url = detail_url(ingredient.id)
+        res = self.client.delete(url)
+        ingredient_exists = Ingredient.objects.filter(user=self.user).exists()
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(ingredient_exists)
